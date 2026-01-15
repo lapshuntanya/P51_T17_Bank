91f4db557ed039a0171f74ce4cb90842c3eb9e44
Tetiana Perederii <lapshun.tatiana@gmail.com>
2026-01-15 17:21:48 +0200

    card

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
-#include <iostream>
+#include "Card.h"
 
 int main() {
+    Card Tetiana;// name = "", money = 0,  number =0
+    Tetiana.createCard();
+
+    Tetiana.showCard();
 
     return 0;
 }
